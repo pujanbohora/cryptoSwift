3603d9f24a4861cfe0c515aad002476449e2f3f9
Pujan Bohora <bohorapawan@gmail.com>
2024-05-09 20:02:48 +0545

    Initial Commit

---
+++ SwiftCrypto/SwiftCryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftCryptoApp.swift
+//  SwiftCrypto
+//
+//  Created by Pawan Bohora on 5/9/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftCryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
